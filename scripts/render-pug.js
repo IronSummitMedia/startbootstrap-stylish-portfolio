0bf1c7d047257a99bfb7ca7f510cc16ffd5000aa
Pirulug <pirulug@gmail.com>
2024-05-19 10:29:25 -0500

    Updated dependencies in package.json and enhanced scripts
    This commit includes updates to the dependencies specified in the package.json file:
    - Bootstrap version was updated to 5.3.3.
    - Versions of development dependencies (devDependencies) such as autoprefixer, browser-sync, chokidar, concurrently, postcss, prettier, and sass were updated to newer versions.
    
    Additionally, enhancements were made to the project scripts:
    - The render-pug.js file was modified to now use promises instead of callbacks, improving code readability and maintainability.
    - The start.js script was updated to handle errors more robustly, now it handles exceptions and displays a success or failure message accordingly.
    
    These updates and enhancements will help keep the project up-to-date and operating more efficiently.

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -1,35 +1,37 @@
-'use strict';
-const fs = require('fs');
-const upath = require('upath');
-const pug = require('pug');
-const sh = require('shelljs');
-const prettier = require('prettier');
+"use strict";
+const fs = require("fs").promises;
+const upath = require("upath");
+const pug = require("pug");
+const sh = require("shelljs");
+const prettier = require("prettier");
 
-module.exports = function renderPug(filePath) {
-    const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
-    const srcPath = upath.resolve(upath.dirname(__filename), '../src');
+module.exports = async function renderPug(filePath) {
+    const destPath = filePath
+        .replace(/src\/pug\//, "dist/")
+        .replace(/\.pug$/, ".html");
+    const srcPath = upath.resolve(upath.dirname(__filename), "../src");
 
     console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
-    const html = pug.renderFile(filePath, {
-        doctype: 'html',
+    const html = await pug.renderFile(filePath, {
+        doctype: "html",
         filename: filePath,
-        basedir: srcPath
+        basedir: srcPath,
     });
 
     const destPathDirname = upath.dirname(destPath);
-    if (!sh.test('-e', destPathDirname)) {
-        sh.mkdir('-p', destPathDirname);
+    if (!sh.test("-e", destPathDirname)) {
+        sh.mkdir("-p", destPathDirname);
     }
 
-    const prettified = prettier.format(html, {
+    const prettified = await prettier.format(html, {
         printWidth: 1000,
         tabWidth: 4,
         singleQuote: true,
-        proseWrap: 'preserve',
-        endOfLine: 'lf',
-        parser: 'html',
-        htmlWhitespaceSensitivity: 'ignore'
+        proseWrap: "preserve",
+        endOfLine: "lf",
+        parser: "html",
+        htmlWhitespaceSensitivity: "ignore",
     });
 
-    fs.writeFileSync(destPath, prettified);
+    await fs.writeFile(destPath, prettified);
 };
